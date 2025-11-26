1ab407226fb94daccc042bd9def6f9edda2de33b
左措 <73263982+kiiyorku@users.noreply.github.com>
2025-11-27 05:56:41 +0800

    1127

--- home/js/manga.js
+++ home/js/manga.js
@@ -240,290 +240,47 @@ $(document).ready(function () {
 
 
 
+// ==========================================
+//   轻量级内容保护 (无损性能版)
+// ==========================================
 
-(function (_0x45fb68, _0x4c00ff) {
-  var _0x13048c = _0x40b7,
-    _0x3299e0 = _0x45fb68();
-  while (!![]) {
-    try {
-      var _0x41971e =
-        parseInt(_0x13048c(0xbe)) / 0x1 +
-        (parseInt(_0x13048c(0xc7)) / 0x2) * (parseInt(_0x13048c(0xbd)) / 0x3) +
-        (-parseInt(_0x13048c(0xab)) / 0x4) *
-          (-parseInt(_0x13048c(0x9b)) / 0x5) +
-        -parseInt(_0x13048c(0xc8)) / 0x6 +
-        (-parseInt(_0x13048c(0xa3)) / 0x7) * (parseInt(_0x13048c(0xb7)) / 0x8) +
-        (-parseInt(_0x13048c(0x98)) / 0x9) *
-          (-parseInt(_0x13048c(0x99)) / 0xa) +
-        (parseInt(_0x13048c(0xa4)) / 0xb) * (-parseInt(_0x13048c(0xb6)) / 0xc);
-      if (_0x41971e === _0x4c00ff) break;
-      else _0x3299e0["push"](_0x3299e0["shift"]());
-    } catch (_0x931042) {
-      _0x3299e0["push"](_0x3299e0["shift"]());
-    }
-  }
-})(_0x3323, 0xcba8e),
-  (function () {
-    var _0xb085f1 = _0x40b7,
-      _0x25c9a1 = (function () {
-        var _0x1cca2d = !![];
-        return function (_0x58f6da, _0x50332f) {
-          var _0x40e493 = _0x1cca2d
-            ? function () {
-                var _0x4492b5 = _0x40b7;
-                if (_0x50332f) {
-                  var _0x73535d = _0x50332f[_0x4492b5(0xad)](
-                    _0x58f6da,
-                    arguments
-                  );
-                  return (_0x50332f = null), _0x73535d;
-                }
-              }
-            : function () {};
-          return (_0x1cca2d = ![]), _0x40e493;
-        };
-      })(),
-      _0x4acc14 = _0x25c9a1(this, function () {
-        var _0x58cbe8 = _0x40b7;
-        return _0x4acc14[_0x58cbe8(0xa2)]()
-          [_0x58cbe8(0xb0)](_0x58cbe8(0xa7))
-          ["toString"]()
-          [_0x58cbe8(0x93)](_0x4acc14)
-          [_0x58cbe8(0xb0)]("(((.+)+)+)+$");
-      });
-    _0x4acc14();
-    var _0x1a4fd1 = (function () {
-      var _0x876f1e = !![];
-      return function (_0x46cf50, _0x548ac1) {
-        var _0x52914d = _0x876f1e
-          ? function () {
-              var _0xe1d4d3 = _0x40b7;
-              if (_0x548ac1) {
-                var _0x3fc1c0 = _0x548ac1[_0xe1d4d3(0xad)](
-                  _0x46cf50,
-                  arguments
-                );
-                return (_0x548ac1 = null), _0x3fc1c0;
-              }
-            }
-          : function () {};
-        return (_0x876f1e = ![]), _0x52914d;
-      };
-    })();
-    (function () {
-      _0x1a4fd1(this, function () {
-        var _0x432e32 = _0x40b7,
-          _0x2c6b24 = new RegExp(_0x432e32(0xc2)),
-          _0x3e6359 = new RegExp(_0x432e32(0xa9), "i"),
-          _0x3cbe5e = _0x30709c("init");
-        !_0x2c6b24["test"](_0x3cbe5e + _0x432e32(0xa5)) ||
-        !_0x3e6359[_0x432e32(0xb4)](_0x3cbe5e + _0x432e32(0xac))
-          ? _0x3cbe5e("0")
-          : _0x30709c();
-      })();
-    })();
-    var _0x46d59d = (function () {
-        var _0x55694a = !![];
-        return function (_0x4f142c, _0x3a1cf0) {
-          var _0x562687 = _0x55694a
-            ? function () {
-                var _0x16708e = _0x40b7;
-                if (_0x3a1cf0) {
-                  var _0x26dc15 = _0x3a1cf0[_0x16708e(0xad)](
-                    _0x4f142c,
-                    arguments
-                  );
-                  return (_0x3a1cf0 = null), _0x26dc15;
-                }
-              }
-            : function () {};
-          return (_0x55694a = ![]), _0x562687;
-        };
-      })(),
-      _0x19a7f6 = _0x46d59d(this, function () {
-        var _0x19b491 = _0x40b7,
-          _0x24b0a7 = function () {
-            var _0x1fdf4e = _0x40b7,
-              _0x534de4;
-            try {
-              _0x534de4 = Function(
-                _0x1fdf4e(0x97) +
-                  "{}.constructor(\x22return\x20this\x22)(\x20)" +
-                  ");"
-              )();
-            } catch (_0x476b6d) {
-              _0x534de4 = window;
-            }
-            return _0x534de4;
-          },
-          _0x50bda4 = _0x24b0a7(),
-          _0x26668b = (_0x50bda4["console"] = _0x50bda4["console"] || {}),
-          _0x4511d3 = [
-            _0x19b491(0x96),
-            _0x19b491(0xc5),
-            _0x19b491(0x9f),
-            _0x19b491(0xc6),
-            _0x19b491(0x9e),
-            _0x19b491(0x9c),
-            _0x19b491(0xb3),
-          ];
-        for (
-          var _0x27c708 = 0x0;
-          _0x27c708 < _0x4511d3[_0x19b491(0xa0)];
-          _0x27c708++
-        ) {
-          var _0x103973 =
-              _0x46d59d[_0x19b491(0x93)][_0x19b491(0xb8)]["bind"](_0x46d59d),
-            _0x1f85ef = _0x4511d3[_0x27c708],
-            _0x150667 = _0x26668b[_0x1f85ef] || _0x103973;
-          (_0x103973[_0x19b491(0xaa)] = _0x46d59d[_0x19b491(0x95)](_0x46d59d)),
-            (_0x103973[_0x19b491(0xa2)] =
-              _0x150667[_0x19b491(0xa2)][_0x19b491(0x95)](_0x150667)),
-            (_0x26668b[_0x1f85ef] = _0x103973);
+(function() {
+    // 1. 全局禁止右键菜单
+    document.oncontextmenu = function(e) {
+        // 如果点击的是特定的可交互区域（可选），可以放行
+        // 但为了保护图片，通常全部禁止
+        e.preventDefault();
+        return false;
+    };
+
+    // 2. 全局禁止拖拽 (防止把图片拖到桌面保存)
+    document.ondragstart = function(e) {
+        return false;
+    };
+
+    // 3. 禁止特定的快捷键 (只禁保存和F12，不影响方向键/滚轮)
+    document.onkeydown = function(e) {
+        // F12
+        if (e.keyCode == 123) {
+            e.preventDefault();
+            return false;
         }
-      });
-    _0x19a7f6();
-    ("use strict");
-    document[_0xb085f1(0xbb)](_0xb085f1(0xc0), function (_0xd5be4c) {
-      var _0x564d94 = _0xb085f1;
-      return _0xd5be4c[_0x564d94(0xaf)](), ![];
-    }),
-      document[_0xb085f1(0xbb)](_0xb085f1(0xc1), function (_0x1c558d) {
-        var _0x374891 = _0xb085f1;
-        return _0x1c558d[_0x374891(0xaf)](), ![];
-      }),
-      document[_0xb085f1(0xbb)](_0xb085f1(0x9d), function (_0x3d367c) {
-        var _0x2ba036 = _0xb085f1;
-        if (_0x3d367c["keyCode"] == 0x7b)
-          return _0x3d367c[_0x2ba036(0xaf)](), ![];
-        if (
-          _0x3d367c[_0x2ba036(0xc3)] &&
-          _0x3d367c["shiftKey"] &&
-          (_0x3d367c[_0x2ba036(0xbf)] == 0x49 ||
-            _0x3d367c[_0x2ba036(0xbf)] == 0x4a ||
-            _0x3d367c[_0x2ba036(0xbf)] == 0x43)
-        )
-          return _0x3d367c[_0x2ba036(0xaf)](), ![];
-        if (_0x3d367c[_0x2ba036(0xc3)] && _0x3d367c["keyCode"] == 0x53)
-          return _0x3d367c["preventDefault"](), alert(_0x2ba036(0xb2)), ![];
-        if (_0x3d367c[_0x2ba036(0xc3)] && _0x3d367c["keyCode"] == 0x55)
-          return _0x3d367c["preventDefault"](), ![];
-      });
-    function _0x1b5f51() {
-      setInterval(function () {
-        var _0x540abd = _0x40b7;
-        (function () {})[_0x540abd(0x93)](_0x540abd(0xb5))();
-      }, 0x3e8);
-    }
-    var _0x8c4bfd = 0xa0,
-      _0x353fd4 = function () {
-        var _0x410251 = _0xb085f1,
-          _0x37ed7d =
-            window[_0x410251(0xc4)] - window["innerWidth"] > _0x8c4bfd,
-          _0x232a6d =
-            window[_0x410251(0xc9)] - window[_0x410251(0xb1)] > _0x8c4bfd;
-        (_0x37ed7d || _0x232a6d) &&
-          (document[_0x410251(0x9a)][_0x410251(0xba)] = _0x410251(0xa1));
-      };
-  })();
-function _0x40b7(_0x55e9dd, _0x452060) {
-  var _0x298e10 = _0x3323();
-  return (
-    (_0x40b7 = function (_0x32ec8c, _0x55d554) {
-      _0x32ec8c = _0x32ec8c - 0x93;
-      var _0x329e15 = _0x298e10[_0x32ec8c];
-      return _0x329e15;
-    }),
-    _0x40b7(_0x55e9dd, _0x452060)
-  );
-}
-function _0x3323() {
-  var _0x1175ee = [
-    "log",
-    "return\x20(function()\x20",
-    "26181JKbYzS",
-    "2570guaGTB",
-    "body",
-    "9385SxlGnk",
-    "table",
-    "keydown",
-    "exception",
-    "info",
-    "length",
-    "<h1>Content\x20Protected</h1>",
-    "toString",
-    "2807pHMTuZ",
-    "8063rnOWjn",
-    "chain",
-    "while\x20(true)\x20{}",
-    "(((.+)+)+)+$",
-    "gger",
-    "\x5c+\x5c+\x20*(?:[a-zA-Z_$][0-9a-zA-Z_$]*)",
-    "__proto__",
-    "2748Uduotd",
-    "input",
-    "apply",
-    "stateObject",
-    "preventDefault",
-    "search",
-    "innerHeight",
-    "Prohibited\x20Action:\x20Save\x20is\x20disabled.",
-    "trace",
-    "test",
-    "debugger",
-    "15684jRyfnk",
-    "3120HiZGBo",
-    "prototype",
-    "action",
-    "innerHTML",
-    "addEventListener",
-    "string",
-    "3hfSEWD",
-    "284945RIeimo",
-    "keyCode",
-    "contextmenu",
-    "dragstart",
-    "function\x20*\x5c(\x20*\x5c)",
-    "ctrlKey",
-    "outerWidth",
-    "warn",
-    "error",
-    "1730898ppbMVx",
-    "7433370KoXoEX",
-    "outerHeight",
-    "constructor",
-    "debu",
-    "bind",
-  ];
-  _0x3323 = function () {
-    return _0x1175ee;
-  };
-  return _0x3323();
-}
-function _0x30709c(_0x2c13e5) {
-  function _0x10a780(_0x29465e) {
-    var _0x2f1b13 = _0x40b7;
-    if (typeof _0x29465e === _0x2f1b13(0xbc))
-      return function (_0x3fbdc3) {}
-        ["constructor"](_0x2f1b13(0xa6))
-        [_0x2f1b13(0xad)]("counter");
-    else
-      ("" + _0x29465e / _0x29465e)[_0x2f1b13(0xa0)] !== 0x1 ||
-      _0x29465e % 0x14 === 0x0
-        ? function () {
-            return !![];
-          }
-            [_0x2f1b13(0x93)](_0x2f1b13(0x94) + _0x2f1b13(0xa8))
-            ["call"](_0x2f1b13(0xb9))
-        : function () {
-            return ![];
-          }
-            [_0x2f1b13(0x93)]("debu" + _0x2f1b13(0xa8))
-            [_0x2f1b13(0xad)](_0x2f1b13(0xae));
-    _0x10a780(++_0x29465e);
-  }
-  try {
-    if (_0x2c13e5) return _0x10a780;
-    else _0x10a780(0x0);
-  } catch (_0x1b0201) {}
-}
+        // Ctrl+S (保存)
+        if (e.ctrlKey && e.keyCode == 83) {
+            e.preventDefault();
+            return false;
+        }
+        // Ctrl+Shift+I (开发者工具)
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+            e.preventDefault();
+            return false;
+        }
+        // Ctrl+U (查看源码)
+        if (e.ctrlKey && e.keyCode == 85) {
+            e.preventDefault();
+            return false;
+        }
+    };
+    
+    // 注意：这里没有写任何死循环代码，滚轮绝对不会卡！
+})();
