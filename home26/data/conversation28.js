78e0cb19dc116b0d85a347c9fbf2e32f4ae4996f
左措 <73263982+kiiyorku@users.noreply.github.com>
2026-02-14 03:05:16 +0800

    0214

--- home26/data/conversation28.js
+++ home26/data/conversation28.js
@@ -69,7 +69,7 @@ export default {
     },
     {
       type: `narration`,
-      text: `シャーロームは近づいてくる彼女を見た。黒石英が警戒しながらシャーロームの前に立ち、通信信号が支援を求め、中心にエネルギーが集まった。`
+      text: `執行人は近づいてくる彼女を見た。黒石英が警戒しながら執行人の前に立ち、通信信号が支援を求め、中心にエネルギーが集まった。`
     },
     {
       type: `dialogue`,
@@ -80,81 +80,81 @@ export default {
     },
     {
       type: `narration`,
-      text: `シャーロームは答えず、血に塗れた業師を見、彼女の真っ赤な瞳を見つめた。`
+      text: `執行人は答えず、血に塗れた業師を見、彼女の真っ赤な瞳を見つめた。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/x3.png`,
-      name: `シャーローム`,
+      name: `執行人`,
       text: `落ち着いているみたいだな。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `私がどんな反応すると思った？泣き崩れる？取り乱す？ふふ、ありえないわ。こんなこと、彼女たちの自業自得よ。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `何度も、何度も言ったよね。実力がないなら、でかい仕事を受けるな。自分を守るだけでいい、私を呼ぶなって。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `……そう言ったのに、馬鹿なことするんだから、どうしようもない。「Garden」の花たちは、生も死も覚悟すべきだった。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
-      text: `ただ、上庭の小娘、お前、今年何歳だ？25？26？上庭の罐から這い出て何年になる？`
+      name: `業師`,
+      text: `ただ、上庭の小娘、君、今年何歳だ？25？26？上庭の罐から這い出て何年になる？`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `あの紙切れを残したのは……私を怒らせたかったのか？`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `私を舐めてるのか？`
     },
     {
       type: `narration`,
-      text: `4、5人のHUSH精鋭が危険を察知し、シャーロームの前に飛び出した。だが、赤い光が閃き、串刺しのように一瞬で貫かれた。`
+      text: `4、5人のHUSH精鋭が危険を察知し、執行人の前に飛び出した。だが、赤い光が閃き、串刺しのように一瞬で貫かれた。`
     },
     {
       type: `narration`,
-      text: `レオポルドはすべての障害を砕き、シャーロームに向かって突進した。瞳は血のように赤く燃えていた。`
+      text: `業師はすべての障害を砕き、執行人に向かって突進した。瞳は血のように赤く燃えていた。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `その程度で小賢しい真似をするつもり!?`
     },
     {
       type: `narration`,
-      text: `レオポルドの足元には残骸が積み重なり、道すがら血が広がっていた。彼女の手の花剣は砕け、脇に投げ捨て、頭を下げて笑いながら呟いた。`
+      text: `業師の足元には残骸が積み重なり、道すがら血が広がっていた。彼女の手の花剣は砕け、脇に投げ捨て、頭を下げて笑いながら呟いた。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `私さ、何度も奪われたよ。この世界、卑劣な奴が多すぎるから。`
     },
     {
@@ -167,39 +167,39 @@ export default {
     },
     {
       type: `narration`,
-      text: `レオポルドはそれらを踏みつけ、シャーロームに向かって一歩ずつ進んだ。`
+      text: `業師はそれらを踏みつけ、執行人に向かって一歩ずつ進んだ。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `初めて、誰かが私の猫を蹴った。年上の死に損ないだった。そいつを押さえつけて、めっちゃ殴った。手にも顔にも消えない傷を残した。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `2回目、誰かが私の家族を奪った。一人でそいつの大邸宅に乗り込んで、偉そうに立ちはだかる奴らを全部片付けた。最後にその大人は私の前に跪いて、泣きながら悔い改めた。声が枯れるまで。そいつを終わらせた。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `3回目、誰かが友達の目を奪った。そいつを真っ暗な地下室に閉じ込めた。たった2ヶ月で、自分で終わらせた。へ……一生続く前に。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
-      text: `4回目、5回目、6回目……お前が何回目かなんて、もう覚えてない。`
+      name: `業師`,
+      text: `4回目、5回目、6回目……君が何回目かなんて、もう覚えてない。`
     },
     {
       type: `narration`,
-      text: `彼女が手を上げると、足元の敗者たちが痙攣し、体内から赤い花芽が引き出され、瞬時に剣に凝縮した。レオポルドはそれを振り上げた。`
+      text: `彼女が手を上げると、足元の敗者たちが痙攣し、体内から赤い花芽が引き出され、瞬時に剣に凝縮した。業師はそれを振り上げた。`
     },
     {
       type: `narration`,
@@ -209,42 +209,42 @@ export default {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `復讐を頼む人たちはめっちゃ苦しんでる。一度だけ、一度だけ倒されただけで。痛みが鮮明で、抗えず、もう立ち直れないって思う。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `ふん、違うよ……何度か経験すればわかる。どんなものでも倒せる。決意して、どんな代償でも払えば。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
-      text: `目には目を、歯には歯を。代償をわからせてやる。そうすれば、誰もが学ぶ――レオポルドのものは絶対に触るな。`
+      name: `業師`,
+      text: `目には目を、歯には歯を。代償をわからせてやる。そうすれば、誰もが学ぶ――業師のものは絶対に触るな。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `私のもの、奪われたものは必ず取り戻す。廃墟になっても私の手に戻す。その後……`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `人は強い。廃墟の上でも育つ。折られた場所に新芽が生え、焼かれた場所に新枝が伸びる。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `私を冒涜した奴らはみんなくそくらえ。そしていつも、私は生まれ変わる。こうやって――`
     },
     {
@@ -253,40 +253,47 @@ export default {
     },
     {
       type: `narration`,
-      text: `レオポルドは惜しまず、左胸を押さえ、引いた。`
+      text: `業師は惜しまず、左胸を押さえ、引いた。`
     },
     {
       type: `narration`,
-      text: `一瞬、鮮紅の力が胸から噴き出し、競うように外へ咲き、手元に集まった。狂厄級能力者が命で凝縮した鮮紅の長剣を、レオポルドは振り、青い光束を断ち切った。`
+      text: `一瞬、鮮紅の力が胸から噴き出し、競うように外へ咲き、手元に集まった。狂厄級能力者が命で凝縮した鮮紅の長剣を、業師は振り、青い光束を断ち切った。`
     },
     {
       type: `narration`,
-      text: `黒石英がより鋭い警告を発し、信号が上庭に直結した。次の瞬間、青い光の雨が天井を突き破り、レオポルドに襲いかかった。彼女は剣を宙に舞わせ、花傘を開くように光雨を防ぎ、姿を消した。`
+      text: `黒石英がより鋭い警告を発し、信号が上庭に直結した。次の瞬間、青い光の雨が天井を突き破り、業師に襲いかかった。彼女は剣を宙に舞わせ、花傘を開くように光雨を防ぎ、姿を消した。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      name: `業師`,
       text: `人の人生を簡単に弄べると思ってる？私を簡単に潰せると？`
     },
     {
       type: `narration`,
-      text: `彼女はシャーロームの前に瞬時に現れ、剣先が地面に血紅の痕を刻んだ。`
+      text: `彼女は執行人の前に瞬時に現れ、剣先が地面に血紅の痕を刻んだ。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/x6.png`,
-      name: `シャーローム`,
-      text: `まさか、業師。君の時間の方が私より長い。`
+      name: `執行人`,
+      text: `滅相もない、業師。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/x6.png`,
-      name: `シャーローム`,
-      text: `運命が何か、君の方がよくわかってる。`
+      name: `執行人`,
+      text: `君の時は私より遥かに長い。`
+    },
+    {
+      type: `dialogue`,
+      position: `left`,
+      avatar: `img/dh/x6.png`,
+      name: `執行人`,
+      text: `運命（さだめ）が何か、君の方がよくわかっているはずだ。`
     },
     {
       type: `dialogue`,
@@ -297,7 +304,7 @@ export default {
     },
     {
       type: `narration`,
-      text: `黒石英は判断を下した。敵対能力者を止められず、即座にシャーロームに照準を合わせた。だが、遅かった。赤い光が黒石英を瞬時に砕き、シャーロームの胸を直線的に貫き、深く突き刺し――`
+      text: `黒石英は判断を下した。敵対能力者を止められず、即座に執行人に照準を合わせた。だが、遅かった。赤い光が黒石英を瞬時に砕き、執行人の胸を直線的に貫き、深く突き刺し――`
     },
     {
       type: `narration`,
@@ -305,29 +312,29 @@ export default {
     },
     {
       type: `narration`,
-      text: `レオポルドは後ろに引き、シャーロームの胸から花を引き抜いた。`
+      text: `業師は後ろに引き、執行人の胸から花を引き抜いた。`
     },
     {
       type: `narration`,
-      text: `花がシャーロームの胸から咲き乱れ、わずかな血色が急速に消えた。魂と命が狂厄となり、激昂する業師に引き抜かれ、レオポルドの手で燃えた。`
+      text: `花が執行人の胸から咲き乱れ、わずかな血色が急速に消えた。魂と命が狂厄となり、激昂する業師に引き抜かれ、業師の手で燃えた。`
     },
     {
       type: `dialogue`,
       position: `left`,
-      avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
-      text: `これでも殺せないんでしょう、上庭の化け物め。なら咲くといいわ……傲慢で非人道的な悪魔がどのような花を咲かせるのか、お前の命がどれだけ醜いのかを見せてもらうわ。`
+      avatar: `img/dh/L_401.png`,
+      name: `業師`,
+      text: `これでも殺せないんでしょう、上庭の化け物め。なら咲くといいわ……傲慢で非人道的な悪魔がどのような花を咲かせるのか、君の命がどれだけ醜いのかを見せてもらうわ。`
     },
     {
       type: `dialogue`,
       position: `left`,
-      avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
+      avatar: `img/dh/L_401.png`,
+      name: `業師`,
       text: `これで私にピッタリの復讐劇を用意できる。思う存分、この怒りを晴らせる！`
     },
     {
       type: `narration`,
-      text: `シャーロームは何もせず、空中で灰と化した黒石英を、胸から溢れる結晶の花を、上庭の封鎖がすべて破壊されるのを見ていた。`
+      text: `執行人は何もせず、空中で灰と化した黒石英を、胸から溢れる結晶の花を、上庭の封鎖がすべて破壊されるのを見ていた。`
     },
     {
       type: `image`,
@@ -339,27 +346,27 @@ export default {
     },
     {
       type: `narration`,
-      text: `シャーロームの顔に、<span>ついに心からの笑</span>みが浮かんだ。`
+      text: `執行人の顔に、<span>ついに心からの笑</span>みが浮かんだ。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/x7.png`,
-      name: `シャーローム`,
+      name: `執行人`,
       text: `これが君が命と引き換えに得た力なんだ。流れるすべての未来を犠牲にして、絶対的な強さと自由を手に入れたんだ。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/x7.png`,
-      name: `シャーローム`,
+      name: `執行人`,
       text: `ここまで辿り着けるのは君だけだ。本当に<span>見事</span>だ。`
     },
     {
       type: `dialogue`,
       position: `left`,
       avatar: `img/dh/x7.png`,
-      name: `シャーローム`,
+      name: `執行人`,
       text: `君がこれほどまでに強く反骨精神を持っていてくれてありがとう……やっぱり君が勝った。`
     },
     {
@@ -377,9 +384,9 @@ export default {
     {
       type: `dialogue`,
       position: `left`,
-      avatar: `img/dh/leopold.png`,
-      name: `レオポルド`,
-      text: `お前が咲かせたのは花じゃない――`
+      avatar: `img/dh/L_401.png`,
+      name: `業師`,
+      text: `君が咲かせたのは花じゃない――`
     },
     {
       type: `narration`,
